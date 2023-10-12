ace037dfdbc8b10a7220e8ad35d24abfca119ebf
AbdulSamadMemon <142597541+AbdulSamadMemon@users.noreply.github.com>
2023-10-12 22:14:33 +0500

    Update user

--- routes/user.js
+++ routes/user.js
@@ -1,52 +1,64 @@
-const abc = require('express');
-const router = abc.Router();
+const express = require('express')
+const router = express.Router()
+const UserModel = require('../Model/user')
 
-const users = [
-    {
-        name : 'Abdul',
-        id : 1
-    },
-    {
-        name : 'Sam',
-        id : 2
-    },
-    {
-        name : 'Sameer',
-        id : 3
-    }
-]
+// const users = [{
+//     name: 'Abdul',
+//     id: 1
+// },
+// {
+//     name: 'Sameer',
+//     id: 2
+// },
+// {
+//     name: 'Noman',
+//     id: 3
+// },
+// {
+//     name: 'Sid',
+//     id: 4
+// },
+// {
+//     name: 'Areeb',
+//     id: 5
+// }
+// ]
 
-// All Users Get
-router.get('/', (req, res)=>{
+router.get('/', async (req, res) => {
+    const users = await UserModel.find()
     res.status(200).send({
-        status : 200,
-        users : users
+        status: 200,
+        users
     })
 })
 
-// Single User Get
-router.get('/:id', (req, res)=>{
-    const user = users.find((data) => data.id == req.params.id)
-    if(!user){
-        res.status(500).send({
-            status : 500,
-            error : true,
-            user : 'User not found'
+router.get('/:id', async (req, res) => {
+    const user = await users.find((data) => data.id == req.params.id)
+    if (!user) {
+        res.send({
+            status: 500,
+            error: true,
+            msg: 'User not found'
         })
     }
-    if(user){
-    res.status(200).send({
-        status : 200,
-        user : user
-    })
-}
+    else {
+        res.send({ status: 200, user })
+    }
 })
 
-//Post Method
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     console.log(req.body)
-    users.push({name: req.body.name, id: users.length + 1})
-    res.status(200).send({status: 200, user: {name: req.body.name, id: users.length + 1}})
+    try{
+    const user = await UserModel.create({...req.body})
+    res.status(200).send({status: 200, user})
+    }
+    catch(err){
+        res.status(500).send({status: 500, error: err, msg: "internal server error"})
+    }
+    // users.push({ name: req.body.name, id: users.length + 1 })
+    // res.send('Post Update')
 })
+
+
 
 module.exports = router
